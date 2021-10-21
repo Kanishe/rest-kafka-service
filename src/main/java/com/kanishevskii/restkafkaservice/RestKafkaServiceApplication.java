106ad85cb79c372b80f4e5bc29df8f49e4e506fb
Kanishe <a.kanishevskii@gmail.com>
2021-10-22 01:07:22 +0300

    added initial structure for the project

---
+++ src/main/java/com/kanishevskii/restkafkaservice/RestKafkaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kanishevskii.restkafkaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestKafkaServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestKafkaServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kanishevskii/restkafkaservice/RestKafkaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kanishevskii.restkafkaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestKafkaServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
